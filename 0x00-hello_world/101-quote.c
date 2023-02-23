3a0fae2d104142b0f5b4c180a38879c6b0ffd31e
Marykouva <marykouvedou@gmail.com>
2023-02-23 09:07:22 -0800

    fixed error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -6,6 +7,7 @@
  */
 int main(void)
 {
-	puts("\"and that piece of art is useful" - Dora ,) Korper ,)2015 - 10 - 19");
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
