0eec2d0cdea99e3d8e5b9625069ea5369c849fd3
Marykouva <marykouvedou@gmail.com>
2023-04-04 10:56:54 -0700

    corrected error

--- 0x13-more_singly_linked-lists/0-print_listint.c
+++ 0x13-more_singly_linked-lists/0-print_listint.c
@@ -1,3 +1,4 @@
+#include "stdio.h"
 #include "lists.h"
 /**
  * print_listint - prints all the elements of a linked list
@@ -10,6 +11,9 @@ size_t print_listint(const listint_t *h)
 	size_t num = 0;
 
 	listint_t *j = h;
+
+	if j == NULL
+	       	return (0);
 
 	while (j != NULL)
 	{
